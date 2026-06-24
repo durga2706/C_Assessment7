681c312becba3eb1e47307c6a47763fe347e9ea3
durga2706 <durkadevi0610@gmail.com>
2026-06-24 20:04:19 +0530

    Create c_problem21.c

---
+++ c_problem21.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void disp_total_odd_digits()
+{
+    int num,digit,count=0;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    while(num>0)
+    {
+        digit=num%10;
+        if(digit%2!=0)
+        {
+            count++;
+        }
+        num=num/10;
+    }
+    printf("%d",count);
+}
+int main()
+{
+    disp_total_odd_digits();
+    return 0;
+}
