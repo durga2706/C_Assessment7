97a49306548238f0f0df410db6754acebebcddb1
durga2706 <durkadevi0610@gmail.com>
2026-07-09 09:51:23 +0530

    Create c_problem28.c

---
+++ c_problem28.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void disp_LCM2();
+int main()
+{
+    disp_LCM2();
+    return 0;
+}
+void disp_LCM2()
+{
+    int a,b,lcm;
+    printf("Enter two numbers:");
+    scanf("%d%d",&a,&b);
+    if (a>b)
+        lcm=a;
+    else
+        lcm=b;
+    while(1)
+    {
+        if (lcm%a==0&&lcm%b==0)
+        {
+            printf("%d",lcm);
+            break;
+        }
+        lcm++;
+    }
+}
